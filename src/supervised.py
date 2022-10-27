9d22efc4070a96feabe7070ba1fe84cd21dfe5e0
Anastasia <nizhegorodova.anastasia@gmail.com>
2022-10-26 23:12:31 -0700

    Modified

--- src/supervised.py
+++ src/supervised.py
@@ -35,7 +35,7 @@ class supervised:
     Returns:
     * Best model from random search
     """
-    def __init__(self, df, estimator, param_distributions, model_name):
+    def __init__(self, df, estimator, model_name):
         X = df[df.columns[:-1]]
         y = df[df.columns[-1]]
         X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8,random_state=0)
@@ -44,7 +44,6 @@ class supervised:
         self.y_train = y_train
         self.y_test =  y_test
         self.estimator = estimator
-        self.params = param_distributions
         self.model_name = model_name
 
     def get_best_model(self,scaled=True):
@@ -58,30 +57,30 @@ class supervised:
             self.X_train = self.X_train_pre
             self.X_test = self.X_test_pre
             print('**Data not scaled**')
-        search = RandomizedSearchCV(self.estimator, param_distributions=self.params, random_state=0,n_jobs=-2,scoring='recall')
+        search = RandomizedSearchCV(self.estimator, random_state=0,n_jobs=-2,scoring='recall')
         search.fit(self.X_train, self.y_train)
         best_model = search.best_estimator_
 
         y_pred = best_model.predict(self.X_test)
         y_pred_train = best_model.predict(self.X_train)
 
-        # Metrics for test data
-        recall = recall_score(self.y_test, y_pred)
-        precision = precision_score(self.y_test, y_pred)
-        f1score = f1_score(self.y_test, y_pred)
-        auc = roc_auc_score(self.y_test, y_pred)
+        # # Metrics for test data
+        # recall = recall_score(self.y_test, y_pred)
+        # precision = precision_score(self.y_test, y_pred)
+        # f1score = f1_score(self.y_test, y_pred)
+        # auc = roc_auc_score(self.y_test, y_pred)
 
-        # Metrics for training data
-        recall_train = recall_score(self.y_train, y_pred_train)
-        precision_train = precision_score(self.y_train, y_pred_train)
-        f1score_train = f1_score(self.y_train, y_pred_train)
-        auc_train = roc_auc_score(self.y_train, y_pred_train)
+        # # Metrics for training data
+        # recall_train = recall_score(self.y_train, y_pred_train)
+        # precision_train = precision_score(self.y_train, y_pred_train)
+        # f1score_train = f1_score(self.y_train, y_pred_train)
+        # auc_train = roc_auc_score(self.y_train, y_pred_train)
 
-        print(f'\n{self.model_name} evaluation metrics: \n\tTest data\tTraining data\t\tDifference')
-        print(f'Recall: \t{100*recall:.2f}%\t\t{100*recall_train:.2f}%\t\t{100*(recall-recall_train):.2f}%')
-        print(f'Precision: \t{100*precision:.2f}%\t\t{100*precision_train:.2f}%\t\t{100*(precision-precision_train):.2f}%')
-        print(f'F1: \t\t{100*f1score:.2f}%\t\t{100*f1score_train:.2f}%\t\t{100*(f1score-f1score_train):.2f}%')
-        print(f'AUC: \t\t{100*auc:.2f}%\t\t{100*auc_train:.2f}%\t\t{100*(auc-auc_train):.2f}%')
+        # print(f'\n{self.model_name} evaluation metrics: \n\tTest data\tTraining data\t\tDifference')
+        # print(f'Recall: \t{100*recall:.2f}%\t\t{100*recall_train:.2f}%\t\t{100*(recall-recall_train):.2f}%')
+        # print(f'Precision: \t{100*precision:.2f}%\t\t{100*precision_train:.2f}%\t\t{100*(precision-precision_train):.2f}%')
+        # print(f'F1: \t\t{100*f1score:.2f}%\t\t{100*f1score_train:.2f}%\t\t{100*(f1score-f1score_train):.2f}%')
+        # print(f'AUC: \t\t{100*auc:.2f}%\t\t{100*auc_train:.2f}%\t\t{100*(auc-auc_train):.2f}%')
         
         print(f'Best model parameters from randomized search: {search.best_params_}')
         ConfusionMatrixDisplay.from_estimator(best_model, self.X_test, self.y_test)
